a5d6847b0e48251fa9bb21d434c34a6f18d3b0ca
heysmtk <smtktom@gmail.com>
2025-05-01 14:06:34 +0200

    add drawable and updatable groups

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import *
 
 def main():
     pygame.init()
@@ -8,17 +10,30 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = (asteroids, updatable, drawable)
+   
+    AsteroidField.containers = (updatable,)
+    asteroid_field = AsteroidField()
     
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-    
-        screen.fill((0, 0, 0))
-        player.draw(screen)
+
+        updatable.update(dt)
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(60) / 1000
-    
+
+
 if __name__ == "__main__":
     main()
