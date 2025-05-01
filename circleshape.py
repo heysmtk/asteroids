9503c40a624104d8f276c28491111a37bf1e12b9
heysmtk <smtktom@gmail.com>
2025-05-01 14:04:06 +0200

    fixed rect variable

--- circleshape.py
+++ circleshape.py
@@ -1,9 +1,7 @@
 import pygame
 
-# Base class for game objects
 class CircleShape(pygame.sprite.Sprite):
     def __init__(self, x, y, radius):
-        # we will be using this later
         if hasattr(self, "containers"):
             super().__init__(self.containers)
         else:
@@ -13,10 +11,12 @@ class CircleShape(pygame.sprite.Sprite):
         self.velocity = pygame.Vector2(0, 0)
         self.radius = radius
 
+        # Tohle přidáme: vytvoření self.rect
+        self.rect = pygame.Rect(0, 0, radius * 2, radius * 2)
+        self.rect.center = self.position
+
     def draw(self, screen):
-        # sub-classes must override
-        pygame.draw.polygon(screen, "white", self.triangle(), 2)
+        pygame.draw.circle(screen, "white", self.position, self.radius, 2)
 
     def update(self, dt):
-        # sub-classes must override
         pass
