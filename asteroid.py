22ffd612db4a316a6d715231532586e42a4259d3
heysmtk <smtktom@gmail.com>
2025-05-07 10:07:21 +0200

    add split() method, splitting destroyed asteroids

--- asteroid.py
+++ asteroid.py
@@ -1,13 +1,29 @@
 import pygame
+import random
 from circleshape import CircleShape
+from constants import ASTEROID_MIN_RADIUS
 
 
 class Asteroid(CircleShape):
-    def __init__(self, x, y, radius):
+    def __init__(self, x, y, radius, velocity=None):
         super().__init__(x, y, radius)
+        self.velocity = velocity
 
     def draw(self, screen):
         pygame.draw.circle(screen, "white", self.position, self.radius, 2)
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        else:
+            random_angle = random.uniform(20, 50)
+            vect1 = self.velocity.rotate(random_angle)
+            vect2 = self.velocity.rotate(-random_angle)
+            new_radius = self.radius - ASTEROID_MIN_RADIUS
+            Asteroid(self.position.x, self.position.y, new_radius, vect1 * 1.2)
+            Asteroid(self.position.x, self.position.y, new_radius, vect2 * 1.2)
